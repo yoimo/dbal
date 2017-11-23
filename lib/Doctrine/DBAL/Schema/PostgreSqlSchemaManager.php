88a4b9b26a291c5f55c20be2d6c4cd91d380e448
NorbertK <norbert@yoimo.no>
2017-11-23 10:33:50 +0100

    Add PostgreSQL 10 support
    https://github.com/doctrine/dbal/issues/2868

--- lib/Doctrine/DBAL/Schema/PostgreSqlSchemaManager.php
+++ lib/Doctrine/DBAL/Schema/PostgreSqlSchemaManager.php
@@ -289,7 +289,15 @@ class PostgreSqlSchemaManager extends AbstractSchemaManager
             $sequenceName = $sequence['relname'];
         }
 
-        $data = $this->_conn->fetchAll('SELECT min_value, increment_by FROM ' . $this->_platform->quoteIdentifier($sequenceName));
+        $version = floatval($this->_conn->getWrappedConnection()->getServerVersion());
+
+        if ($version >= 10) {
+            $data = $this->_conn->fetchAll('SELECT min_value, increment_by FROM pg_sequences WHERE schemaname = \'public\' AND sequencename = '.$this->_conn->quote($sequenceName));
+        }
+        else
+        {
+            $data = $this->_conn->fetchAll('SELECT min_value, increment_by FROM ' . $this->_platform->quoteIdentifier($sequenceName));
+        }
 
         return new Sequence($sequenceName, $data[0]['increment_by'], $data[0]['min_value']);
     }
